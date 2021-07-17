237a7e9442c27f78d8db8ab3e7232fed6398b86f
Shreyas Raviprasad <shreyasraviprasad457@gmail.com>
2021-07-17 18:50:30 +0530

    ver 1

---
+++ agent.py
@@ -0,0 +1,101 @@
+import numpy as np
+import random
+
+
+class Agent():
+	def __init__(self, reward, episodes=1, maxsteps=10, lr=0.7, discount=0.99, explore=1, decay=0.01, minrate=0.01):
+		size = reward.shape[0]
+		self.reward = reward
+		self.qtable = np.zeros((size**2, 4))
+		self.episodes = episodes
+		self.maxsteps = maxsteps
+		self.lr = lr
+		self.discount = discount
+		self.exploration_rate = explore
+		self.decay_rate = decay
+		self.min_exploration_rate = minrate
+
+
+	def get_index(self, current_state):
+		ind = 0
+		size = self.reward.shape[0]
+		for i in range(size):
+			for j in range(size):
+				if ([i, j] == current_state).all():
+					return ind
+				ind += 1
+
+
+	def validate(self, action, current_state):
+		if action == 0:	# left
+			new_state = np.add(np.array(current_state), np.array([0, -1]))
+		elif action == 1: # up
+			new_state = np.add(np.array(current_state), np.array([-1, 0]))
+		elif action == 2:	# right
+			new_state = np.add(np.array(current_state), np.array([0, 1]))
+		elif action == 3: # down
+			new_state = np.add(np.array(current_state), np.array([1, 0]))
+
+		i, j = new_state
+		size = self.reward.shape[0]
+
+		if i in range(size) and j in range(size):
+			if self.reward[i, j] == -10:
+				return [-1]
+			return new_state
+		return [-1]
+
+
+	def train(self):
+		alpha = self.lr
+		gamma = self.discount
+		exploration_rate = self.exploration_rate
+		minrate = self.min_exploration_rate
+		decay = self.decay_rate
+		path = list()
+		episode_rewards = list()
+
+		for episode in range(self.episodes):
+			path.append([])
+
+			current_state = np.array([0, 0])
+			rewards = 0
+
+			for step in range(self.maxsteps):
+				index = self.get_index(current_state)
+				threshold = random.uniform(0, 1)
+				new_state = [-1]
+				valid = True
+				
+				while new_state[0] == -1:
+					if threshold > exploration_rate:
+						actions = self.qtable[index, :]
+						if valid == False:
+							actions[action] = -100
+						action = np.argmax(actions)
+					else:
+						action = random.choice([0,1,2,3])
+
+					new_state = self.validate(action, current_state)
+					if new_state[0] == -1:
+						valid = False
+
+				q = self.qtable[index, action]
+				i, j = new_state
+				rt = self.reward[i, j]
+				new_index = self.get_index(new_state)
+				max_q = np.max(self.qtable[new_index, :])
+				path[episode].append(list(new_state))
+
+				self.qtable[index, action] = (1-alpha)*q + alpha*(rt + gamma*(max_q))
+				current_state = new_state
+				rewards += rt
+
+				if rt >= 10:				
+					break
+
+			episode_rewards.append(rewards)
+			exploration_rate = minrate + (1 - minrate) * np.exp(-decay*episode)
+		
+		self.paths = np.array(path)
+		self.episode_rewards = episode_rewards
---
+++ main.py
@@ -0,0 +1,75 @@
+from agent import Agent
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import time
+import streamlit as st
+
+
+def show_path(paths, reward, episode_reward):
+	r = np.copy(reward)
+
+	fig = plt.figure(figsize=(3, 2))
+	ax = fig.add_subplot(111)
+	plt.axis('off')
+
+	grid_holder = st.empty()
+	grid = ax.imshow(r, cmap='tab20c', interpolation='nearest')
+
+	index = 0
+	for episode in paths[::10]:
+		episode = np.insert(episode, 0, [0, 0], axis=0)
+		for i, j in episode:
+			r = np.copy(reward)
+			r[i, j] = -4
+
+			grid.set_data(r)
+			plt.title(f"Episode:{index}  Reward:{episode_reward[index]}")
+			plt.draw()
+			grid_holder.pyplot(fig)
+			time.sleep(0.01)
+
+		time.sleep(1)
+		index += 10
+
+
+def main():
+
+	reward5 = np.array([[-1, -10, -1, -1, -1],
+					   [-1, -1, -1, -10, -1],
+					   [-1, -10, -10, -10, -1],
+					   [-1, -10, 10, -1, -1],
+					   [-1, -1, -1, -1, -1]])
+
+	reward4 = np.array([[-1, -10, -1, -1],
+					   [-1, -1, -1, -1],
+					   [-10, -10, -10, -1],
+					   [-10, 10, -1, -1]])
+
+	reward3 = np.array([[-1, -10, -1],
+					   [-1, -1, -1],
+					   [-1, -10, 10]])
+
+	st.sidebar.write("# Gridworld\n")
+
+	reward = st.sidebar.selectbox("Choose Grid", ["5x5", "4x4", "3x3"])
+	episodes = st.sidebar.slider("Number of Episodes", 1, 1000, 100, 10)
+	maxsteps = st.sidebar.slider("Max Steps per Episode", 10, 100, 10, 10)
+	lr = st.sidebar.slider("Learning Rate", 0.01, 1.0, 0.70, 0.1)
+	decay = st.sidebar.slider("Decay Rate", 0.01, 1.0, 0.01, 0.01)
+
+	if reward == "5x5":
+		reward = reward5
+	elif reward == "4x4":
+		reward = reward4
+	else:
+		reward = reward3
+	
+	if st.sidebar.button('Train Agent'):
+		agent = Agent(reward, episodes=episodes, maxsteps=maxsteps, lr=lr, decay=decay)
+		agent.train()
+		show_path(agent.paths, reward, agent.episode_rewards)
+
+
+if __name__ == "__main__":
+	main()
